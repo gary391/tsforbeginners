dc9c732586306ec943ac69f72dccee576fd26252
Gaurav Yadav <gary391@gmail.com>
2024-06-21 11:11:41 -0700

    Inheritance in typescript

--- 09_inheritance/inhertanceints.js
+++ 09_inheritance/inhertanceints.js
@@ -1,29 +1,32 @@
 /*
 Inheritance is the process defining a new object using an existing object
 
-Using extends key word
+Using extends key word we inherit
 
-- Accessing existing objects functionality
+- Accessing existing objects functionality example iphone 7 extends iphone6S + increase functionality
 - Updating existing objects functionality
-- Reusability and IS-A relation
+- Reusability and IS-A relation (re-use)
                     
                            VEHICLE
                         /           \
-                  IS-A /             \
+                  IS-A /             \ IS-A
                       /               \
                     CAR               BUS
-             IS-A /     \            /    \
+             IS-A /     \            /    \ IS-A
                  /       \          /      \
                BMW      AUDI      VOLVO   BENZ
 
 
 Here we can inherit properties for Vehicle class to car and bus classes, and similarly
-individual car and bus (companie/make) can inherit from the car and bus classes respectively
+individual car and bus (company/make) can inherit from the car and bus classes respectively
 
 BMW IS-A CAR
 CAR IS-A VEHICLE
+BUS IS-A VEHICLE
 Can't say AUDI IS-A BUS ?
-               */
+
+Note: "IS-A" a term we use to refer to inheritance
+*/
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -39,6 +42,7 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+// Parent class 
 var BMW = /** @class */ (function () {
     // Constructor function, note this refer to the object that will be created
     function BMW(make, model, year) {
@@ -63,9 +67,14 @@ var ThreeSeries = /** @class */ (function (_super) {
     __extends(ThreeSeries, _super);
     // constructor in the child class 
     // Use of super function as the first line of the constructor of the child class
-    function ThreeSeries(make, model, year, cruiseControlEnabled) {
-        var _this = _super.call(this, make, model, year) || this;
+    function ThreeSeries(make, model, year, cruiseControlEnabled, xmRadio) {
+        var _this = 
+        // super is used to call the constructor of the parent class.
+        // Note: super must be the first line of the constructor of the child class
+        // It should pass in all the properties of the parent class.
+        _super.call(this, make, model, year) || this;
         _this.cruiseControlEnabled = cruiseControlEnabled;
+        _this.xmRadio = xmRadio;
         return _this;
     }
     // Method in child class, called cruiseControl that will set the flag to enabled
@@ -76,6 +85,14 @@ var ThreeSeries = /** @class */ (function (_super) {
     ThreeSeries.prototype.cruiseControlStop = function () {
         this.cruiseControlEnabled = false;
         console.log("Disengage CruiseControl!!!");
+    };
+    ThreeSeries.prototype.xmRadioStart = function () {
+        this.xmRadio = true;
+        console.log("XM Radio Started");
+    };
+    ThreeSeries.prototype.xmRadioStop = function () {
+        this.xmRadio = false;
+        console.log("XM Radio Stopped");
     };
     // These method are also present in the parent class, but have been overridden in the child class.
     ThreeSeries.prototype.start = function () {
@@ -88,9 +105,11 @@ var ThreeSeries = /** @class */ (function (_super) {
 }(BMW));
 var FiveSeries = /** @class */ (function (_super) {
     __extends(FiveSeries, _super);
-    function FiveSeries(make, model, year, parkingAssistEnabled) {
+    function FiveSeries(make, model, year, parkingAssistEnabled, laneAssistEabled, onstar) {
         var _this = _super.call(this, make, model, year) || this;
         _this.parkingAssistEnabled = parkingAssistEnabled;
+        _this.laneAssistEabled = laneAssistEabled;
+        _this.onStar = onstar;
         return _this;
     }
     FiveSeries.prototype.parkingAssistStart = function () {
@@ -101,9 +120,22 @@ var FiveSeries = /** @class */ (function (_super) {
         this.parkingAssistEnabled = false;
         console.log("Disengage Parking Assitance");
     };
-    // These method are also present in the parent class, but have been overridden in the child class.
-    // Note in order to override the method, they need to exactly same as the parent class i.e. should have
-    // same number of parameters etc.
+    FiveSeries.prototype.laneAssistStart = function () {
+        this.laneAssistEabled = true;
+        console.log("Lane Assist Enabled");
+    };
+    FiveSeries.prototype.laneAssistStop = function () {
+        this.laneAssistEabled = false;
+        console.log("Disengage Lane Assist");
+    };
+    FiveSeries.prototype.onStarEmergencyAlwaysOn = function () {
+        this.onStar = true;
+        console.log("Emergency Always On");
+    };
+    // These method are also present in the parent class, but have been 
+    // overridden in the child class. Note in order to override the method,
+    // they need to exactly same as the parent class i.e. should have
+    // same type & number of parameters etc.
     FiveSeries.prototype.start = function () {
         console.log("Remote Start");
     };
@@ -112,23 +144,129 @@ var FiveSeries = /** @class */ (function (_super) {
     };
     return FiveSeries;
 }(BMW));
-var threeSeries = new ThreeSeries("BMW", "328i", 2018, false);
+var threeSeries = new ThreeSeries("BMW", "328i", 2018, false, false);
+// You can access the properties of the parent class using the object of the child class.
+// As those properties are inhereted from the parent class.
 console.log(threeSeries.cruiseControlEnabled);
 console.log(threeSeries.make);
 console.log(threeSeries.model);
 console.log(threeSeries.year);
-threeSeries.commonEngineFunc();
+console.log("xmRadio: ".concat(threeSeries.xmRadio));
+threeSeries.commonEngineFunc(); // functionality inhereted from the parent class 
 threeSeries.start();
 threeSeries.stop();
 threeSeries.cruiseControlStart();
 threeSeries.cruiseControlStop();
-var fiveSeries = new FiveSeries("BMW", "535", 2018, false);
+threeSeries.xmRadioStart();
+threeSeries.xmRadioStop();
+console.log("-----------------------");
+console.log("-----------------------");
+var fiveSeries = new FiveSeries("BMW", "535", 2018, false, false, false);
 console.log(fiveSeries.parkingAssistEnabled);
 console.log(fiveSeries.make);
 console.log(fiveSeries.model);
 console.log(fiveSeries.year);
+console.log("laneAssitEnabled: ".concat(fiveSeries.laneAssistEabled));
+console.log("onStar: ".concat(fiveSeries.onStar));
 fiveSeries.commonEngineFunc();
 fiveSeries.start();
 fiveSeries.stop();
 fiveSeries.parkingAssistStart();
 fiveSeries.parkingAssistStop();
+fiveSeries.laneAssistStart();
+// fiveSeries.laneAssistStop();
+fiveSeries.onStarEmergencyAlwaysOn();
+console.log("-----------------------");
+console.log("-----------------------");
+// This is a parent class 
+var Audi = /** @class */ (function () {
+    //Parent class constructor
+    function Audi(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    // Methods
+    Audi.prototype.commonEngineFunc = function () {
+        console.log("Common Func");
+    };
+    Audi.prototype.start = function () {
+        console.log("Start");
+    };
+    Audi.prototype.stop = function () {
+        console.log("Stop");
+    };
+    return Audi;
+}());
+// extend this class using extends key word
+var Q3Suv = /** @class */ (function (_super) {
+    __extends(Q3Suv, _super);
+    // constructor in the child class 
+    function Q3Suv(make, model, year, cruiseControlEnabled) {
+        var _this = 
+        // When we do inheritence the first line in the constructor 
+        // for a child class is super, which is a way of invoking the parent class 
+        // constructor, so we need to pass in all the properties here as well.
+        _super.call(this, make, model, year) || this;
+        _this.cruiseControlEnabled = cruiseControlEnabled;
+        return _this;
+    }
+    // Create a method that can control the properties.
+    Q3Suv.prototype.cruiseControl = function () {
+        this.cruiseControlEnabled = true;
+        console.log("Crusing");
+    };
+    //Overriding Inheritance
+    Q3Suv.prototype.start = function () {
+        console.log("Button Start");
+    };
+    Q3Suv.prototype.stop = function () {
+        console.log("Button Stop");
+    };
+    return Q3Suv;
+}(Audi));
+var Q5Suv = /** @class */ (function (_super) {
+    __extends(Q5Suv, _super);
+    function Q5Suv(make, model, year, parkingAssistEnabled) {
+        var _this = 
+        // When we do inheritence the first line in the constructor 
+        // for a child class is super, which is a way of invoking the parent class 
+        // constructor, so we need to pass in all the properties here as well.
+        _super.call(this, make, model, year) || this;
+        _this.parkingAssistEnabled = parkingAssistEnabled;
+        return _this;
+    }
+    Q5Suv.prototype.passAssist = function () {
+        this.parkingAssistEnabled = true;
+        console.log("Parking");
+    };
+    Q5Suv.prototype.start = function () {
+        console.log("Remote Start");
+    };
+    Q5Suv.prototype.stop = function () {
+        console.log("Remote Stop");
+    };
+    return Q5Suv;
+}(Audi));
+var q3suv = new Q3Suv("Audi", "Q3S", 2024, false);
+console.log(q3suv.cruiseControlEnabled); // If we don't initialize this, we will get undefined, using a constructor.
+// Here we are access the properties of the parent class as well. 
+console.log(q3suv.make);
+console.log(q3suv.model);
+console.log(q3suv.year);
+q3suv.commonEngineFunc();
+q3suv.start();
+q3suv.stop();
+q3suv.cruiseControl();
+console.log("-----------------------");
+console.log("-----------------------");
+var q5suv = new Q5Suv("Audi", "Q5S", 2025, true);
+console.log(q5suv.parkingAssistEnabled); // If we don't initialize this, we will get undefined, using a constructor.
+// Here we are access the properties of the parent class as well. 
+console.log(q5suv.make);
+console.log(q5suv.model);
+console.log(q5suv.year);
+q5suv.commonEngineFunc();
+q5suv.start();
+q5suv.stop();
+q5suv.passAssist();
--- 09_inheritance/inhertanceints.ts
+++ 09_inheritance/inhertanceints.ts
@@ -3,7 +3,7 @@ Inheritance is the process defining a new object using an existing object
 
 Using extends key word we inherit 
 
-- Accessing existing objects functionality
+- Accessing existing objects functionality example iphone 7 extends iphone6S + increase functionality
 - Updating existing objects functionality
 - Reusability and IS-A relation (re-use)
                     
@@ -18,7 +18,7 @@ Using extends key word we inherit
 
 
 Here we can inherit properties for Vehicle class to car and bus classes, and similarly 
-individual car and bus (companie/make) can inherit from the car and bus classes respectively 
+individual car and bus (company/make) can inherit from the car and bus classes respectively 
 
 BMW IS-A CAR
 CAR IS-A VEHICLE
@@ -28,7 +28,7 @@ Can't say AUDI IS-A BUS ?
 Note: "IS-A" a term we use to refer to inheritance
 */
 
-
+// Parent class 
 class BMW {
     make:string;
     model:string;
@@ -184,3 +184,125 @@ fiveSeries.parkingAssistStop();
 fiveSeries.laneAssistStart();
 // fiveSeries.laneAssistStop();
 fiveSeries.onStarEmergencyAlwaysOn();
+
+console.log("-----------------------")
+console.log("-----------------------")
+
+// This is a parent class 
+class Audi {
+    make:string;
+    model:string;
+    year:number;
+
+    //Parent class constructor
+    constructor(make:string, model:string, year:number){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+
+    }
+
+    // Methods
+    commonEngineFunc(){
+        console.log("Common Func")
+    }
+    start(){
+        console.log("Start");
+    }
+    stop(){
+        console.log("Stop");
+    }
+
+}
+
+// extend this class using extends key word
+class Q3Suv extends Audi {
+    // Note there is no rule enforcement as in case of interface 
+    // but we are extending it i.e. adding functionality.
+    // The parent class properties will come to it automatically
+    cruiseControlEnabled:Boolean;
+
+    // Create a method that can control the properties.
+    cruiseControl(){
+        this.cruiseControlEnabled = true;
+        console.log("Crusing");
+    }
+
+    // constructor in the child class 
+    constructor(make:string, model:string, year:number, cruiseControlEnabled:boolean){
+        // When we do inheritence the first line in the constructor 
+        // for a child class is super, which is a way of invoking the parent class 
+        // constructor, so we need to pass in all the properties here as well.
+        super(make,model,year);
+        this.cruiseControlEnabled = cruiseControlEnabled;
+
+    }
+    // Overriding Inheritance
+    // For override the function, these should match what we have in the parent class. 
+    // Example if the parent class function has a paramter, then we should have same parameters
+    // in the child class.
+
+    // In case of new parameters, these functions will be considered as new functions and 
+    // not considered as overriding.
+    start(){
+        console.log("Button Start");
+    }
+    stop(){
+        console.log("Button Stop");
+    }
+   
+    }
+
+
+
+class Q5Suv extends Audi {
+    
+    parkingAssistEnabled:boolean;
+
+    passAssist(){
+        this.parkingAssistEnabled = true;
+        console.log("Parking")
+    }
+
+
+
+    constructor(make:string, model:string, year:number, parkingAssistEnabled:boolean){
+        // When we do inheritence the first line in the constructor 
+        // for a child class is super, which is a way of invoking the parent class 
+        // constructor, so we need to pass in all the properties here as well.
+        super(make,model,year);
+        this.parkingAssistEnabled = parkingAssistEnabled;
+    }
+    start(){
+        console.log("Remote Start");
+    }
+    stop(){
+        console.log("Remote Stop");
+    }
+
+}
+
+var q3suv = new Q3Suv("Audi", "Q3S", 2024, false);
+console.log(q3suv.cruiseControlEnabled); // If we don't initialize this, we will get undefined, using a constructor.
+// Here we are access the properties of the parent class as well. 
+console.log(q3suv.make);
+console.log(q3suv.model);
+console.log(q3suv.year);
+q3suv.commonEngineFunc();
+q3suv.start();
+q3suv.stop();
+q3suv.cruiseControl();
+
+console.log("-----------------------")
+console.log("-----------------------")
+
+var q5suv = new Q5Suv("Audi", "Q5S", 2025, true);
+console.log(q5suv.parkingAssistEnabled); // If we don't initialize this, we will get undefined, using a constructor.
+// Here we are access the properties of the parent class as well. 
+console.log(q5suv.make);
+console.log(q5suv.model);
+console.log(q5suv.year);
+q5suv.commonEngineFunc();
+q5suv.start(); // Here we have overriding this method which is present in the parent class.
+q5suv.stop();
+q5suv.passAssist();
